05e9cdd480a16a68b0683c9ef2d03752dfbe88b8
rajnish.oss <rajnishpandey844@gmail.com>
2025-05-23 08:15:58 +0530

    whatwg-url

--- frontend/src/redux/axiosInstance.js
+++ frontend/src/redux/axiosInstance.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://192.168.1.102:8800/api/',
+  baseURL: 'http://192.168.1.103:8800/api/',
   withCredentials: true,
 });
 
