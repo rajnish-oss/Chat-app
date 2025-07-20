3fbf169398f004811832040314802cd02572dfc7
rajnish-oss <rajnishpandey844@gmail.com>
2025-07-20 22:39:31 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
     port:3000,
     proxy:{
       "/api":{
-        target:["http://localhost:8800","https://bo-chat.onrender.com"],
+        target:["http://localhost:8800","https://bo-chat-backend.vercel.app"],
         changeOrigin:true,
       }
     },
