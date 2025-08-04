d4892bb12084bf89195ca27b4676b1cf6a6f321c
rajnish-oss <rajnishpandey844@gmail.com>
2025-08-04 21:08:31 +0530

    Update Socket.jsx

--- frontend/src/components/Socket.jsx
+++ frontend/src/components/Socket.jsx
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const URL = "https://bo-chat.onrender.com"; 
+const URL = "https://chat-app-v34y.onrender.com"; 
 export const Socket = io(URL,{
   withCredentials:true
 });
