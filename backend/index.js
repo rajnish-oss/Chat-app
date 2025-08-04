90ddc172c16fbc7bb34941c2a5a879e56f991c0f
rajnish-oss <rajnishpandey844@gmail.com>
2025-08-04 16:35:52 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ app.use(cookieParser())
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors({
-    origin:["http://localhost:3000","https://bo-chat-six.vercel.app"],
+    origin:["http://localhost:3000","https://chat-app-xi-ten-95.vercel.app/"],
     methods:["GET","POST","DELETE","PUT"],
     credentials: true,
 }))
@@ -28,7 +28,7 @@ app.use("/api",routes)
 
 const io = new Server(server,{
     cors:{
-        origin:["http://localhost:3000","https://bo-chat-six.vercel.app"],
+        origin:["http://localhost:3000","https://chat-app-xi-ten-95.vercel.app/"],
         credentials:true
     }}
 )
